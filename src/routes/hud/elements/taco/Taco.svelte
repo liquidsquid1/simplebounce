f9cc8b1ecf2b57ae9eb26eb60233b07dace145e8
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-08-11 18:58:03 +0100

    [+] intro message

--- src/routes/hud/elements/taco/Taco.svelte
+++ src/routes/hud/elements/taco/Taco.svelte
@@ -1,33 +1,45 @@
-<script lang="ts">
-    let left = -100;
-    setInterval(() => {
-        left += 100;
+<div class="window">
+  <div class="titlebar">
+    <span class="title">Thanks for using SimpleBounce</span>
+  </div>
+  <div class="content">
+    <p>If you enjoy this theme, be sure to star it on Github</p>
+    <p>You may close this message by turning off the "Taco" section in the HUD module (look at the cool new ClickGUI while you're at it)</p>
 
-        if (left > window.innerWidth + 100) {
-            left = -100;
-        }
-    }, 500);
-</script>
-
-<div class="taco">
-    <img src="img/hud/taco/taco.gif" alt="taco no load :(("
-         class:transition={left < window.innerWidth + 100 && left > -100} style="left: {left}px">
+    <h1>v0.0.0</h1>
+    <p>Wow, didn't know you were here this early. Welp, I've just been adding a few things here and there so its best to check back on the github for new commits.</p>
+  </div>
 </div>
 
 <style lang="scss">
-  .taco {
-    position: relative;
-    height: 60px;
-    background-color: red;
 
-    img {
-      position: absolute;
-      height: 100%;
+  @import "../../../../colors.scss";
 
-      &.transition {
-        transition: linear left 500ms;
-      }
-    }
+  .window {
+    width: 480px;
+    font-family: "Minecraftia";
   }
-</style>
 
+  .titlebar {
+    background-color: rgb(12, 12, 12);
+    border-top: 1px solid $accent-color-1;
+    color: white;
+    padding: 10px;
+    font-size: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .titlebar .title {
+    margin: 0;
+  }
+
+  .content {
+    padding: 20px;
+    background-color: rgb(24, 24, 24);
+    color: white;
+    border-bottom: 1px solid $accent-color-2;
+  }
+
+</style>
