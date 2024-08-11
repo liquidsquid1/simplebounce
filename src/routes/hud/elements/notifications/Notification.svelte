7132df4f2c6b8c0d2bc1fee04d9a08f53b47c6b2
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-08-11 13:56:07 +0100

    [+] notifications

--- src/routes/hud/elements/notifications/Notification.svelte
+++ src/routes/hud/elements/notifications/Notification.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
     export let title: string;
     export let message: string;
-    export let severity: string;
 </script>
 
 <div class="notification">
-    <div class="icon {severity.toString().toLowerCase()}"/>
     <div class="title">{title}</div>
     <div class="message">{message}</div>
 </div>
@@ -14,86 +12,26 @@
   .notification {
     display: grid;
     grid-template-areas:
-            "a b"
-            "a c";
+            "a b";
     grid-template-columns: max-content 1fr;
+    border-left: 2px solid rgb(127, 0, 255);
     column-gap: 10px;
-    background: rgba(0, 0, 0, 0.68);
-    border-radius: 5px;
+    background: rgba(24, 24, 24, 1.0);
     width: 300px;
     overflow: hidden;
     padding: 10px;
     margin-bottom: 10px;
   }
 
-  .icon {
-    height: 40px;
-    width: 40px;
-    background-position: center;
-    background-repeat: no-repeat;
-    border-radius: 4px;
-    grid-area: a;
-    transition: background-color 0.2s;
-    position: relative;
-
-    &.success {
-      background-color: #4dac68;
-      background-image: url("/img/hud/notification/icon-success.svg");
-    }
-
-    &.error {
-      background-color: #fc4130;
-      background-image: url("/img/hud/notification/icon-error.svg");
-    }
-
-    &.info {
-      background-color: #4677ff;
-      background-image: url("/img/hud/notification/icon-info.svg");
-    }
-
-    &.disabled,
-    &.enabled {
-      &::after {
-        content: "";
-        position: absolute;
-        height: 10px;
-        width: 10px;
-        border-radius: 5px;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        background: white;
-        transition: all 0.2s ease-out;
-      }
-
-      background-image: url("/img/hud/notification/icon-toggle.svg");
-    }
-
-    &.enabled {
-      background-color: #4dac68;
-
-      &::after {
-        left: 62%;
-      }
-    }
-
-    &.disabled {
-      background-color: #fc4130;
-
-      &::after {
-        left: 38%;
-      }
-    }
-  }
-
   .title {
-    grid-area: b;
-    font-size: 14px;
+    grid-area: a;
+    font-size: 12px;
     color: white;
-    font-weight: 600;
+    text-shadow: rgba(128,0,255,0.9) 0px 0px 12px;
   }
 
   .message {
-    grid-area: c;
+    grid-area: b;
     font-size: 12px;
     color: #cbd1e3;
   }
