eb632c020c15bf07e53b6807ba8a83369641677b
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-08-11 18:42:51 +0100

    [feat] animated watermark

--- src/routes/hud/elements/Watermark.svelte
+++ src/routes/hud/elements/Watermark.svelte
@@ -6,12 +6,18 @@
 
     @import "../../../colors.scss";
 
+    @keyframes cycle {
+        0% {color: $accent-color-1}
+        50% {color: $accent-color-2}
+        100% {color: $accent-color-1}
+    }
+
     .watermark {
         font-size: 24px;
         color: white;
         text-shadow: 3px 3px #000000;
     }
     .coloured {
-        color: $accent-color-1;
+        animation: linear infinite 2s cycle;
     }
 </style>
