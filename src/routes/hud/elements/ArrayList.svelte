1c4814c25075cbbbe51ead6e86bd1098fb691f4b
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-08-18 19:54:09 +0300

    feat: arraylist fade (#12)
    implements arraylist fade and adds ids

--- src/routes/hud/elements/ArrayList.svelte
+++ src/routes/hud/elements/ArrayList.svelte
@@ -46,14 +46,77 @@
     listen("refreshArrayList", async () => {
         await updateEnabledModules();
     });
+
+    let speed = 25;
+
+    interface RGBColor {
+        r: number;
+        g: number;
+        b: number;
+    }
+
+    let progress = 0;
+    const tSpeed = (0.1 / 20) * speed;
+
+    export function arraylistGradient() {
+        const arraylist = document.getElementById("arraylist");
+        if (arraylist == null) return;
+        var color1 = window.getComputedStyle(arraylist).getPropertyValue("--accent-color-1").trim();
+        var color2 = window.getComputedStyle(arraylist).getPropertyValue("--accent-color-2").trim();
+        const modules = arraylist.children as HTMLCollectionOf<HTMLElement>;
+        for (let i = 0; i < modules.length; i++) {
+            const element = modules[i];
+            if (element.id != "module") continue;
+            const percentage = 1 - (i / modules.length) + (.5 * Math.sin(.5 * i + progress));;
+            const rgb = colorInterpolate(color1, color2, percentage);
+            element.style.color = `rgb( ${rgb.r}, ${rgb.g}, ${rgb.b})`;
+            element.style.borderRight = `2px solid rgb( ${rgb.r}, ${rgb.g}, ${rgb.b})`;
+        }
+
+        progress += tSpeed;
+        if (progress >= Math.PI * 2) {
+            progress = 0;
+        }
+
+    }
+
+    export function getRgb(color: string): RGBColor {
+        const match = color.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        if (!match) {
+          throw new Error(`Invalid color format: ${color}`);
+        }
+        return {
+          r: parseInt(match[1]),
+          g: parseInt(match[2]),
+          b: parseInt(match[3]),
+        };
+    }
+    // https://stackoverflow.com/questions/66123016/interpolate-between-two-colours-based-on-a-percentage-value
+    export function colorInterpolate(colorA: string, colorB: string, interpolation: number): RGBColor {
+        const rgbA = getRgb(colorA);
+        const rgbB = getRgb(colorB);
+
+        interpolation = Math.min(1, Math.max(0, interpolation));
+
+        const interpolateComponent = (prop: keyof RGBColor) =>
+          Math.round(rgbA[prop] * (1 - interpolation) + rgbB[prop] * interpolation);
+
+        return {
+          r: interpolateComponent('r'),
+          g: interpolateComponent('g'),
+          b: interpolateComponent('b'),
+        };
+    }
+
+    setInterval(arraylistGradient, speed)
 </script>
 
-<div class="arraylist">
+<div class="arraylist" id="arraylist">
     {#each enabledModules as {name, tag} (name)}
-        <div class="module" animate:flip={{ duration: 200 }} in:fly={{ x: 50, duration: 200 }}>
+        <div class="module" id="module" animate:flip={{ duration: 200 }} in:fly={{ x: 50, duration: 200 }}>
             {$spaceSeperatedNames ? convertToSpacedString(name) : name}
             {#if tag}
-                <span class="tag"> {tag}</span>
+                <span class="tag" id="tag"> {tag}</span>
             {/if}
         </div>
     {/each}
@@ -61,6 +124,11 @@
 
 <style lang="scss">
   @import "../../../colors.scss";
+
+  :root {
+    --accent-color-1: #{$accent-color-1};
+    --accent-color-2: #{$accent-color-2};
+  }
 
   .module {
     text-transform: lowercase;
