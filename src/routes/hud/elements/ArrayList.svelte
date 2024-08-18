6849b1d251a07277497f0a22262cb69d90956406
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-08-18 15:01:12 +0300

    add script

--- src/routes/hud/elements/ArrayList.svelte
+++ src/routes/hud/elements/ArrayList.svelte
@@ -46,6 +46,68 @@
     listen("refreshArrayList", async () => {
         await updateEnabledModules();
     });
+
+    let speed = 10;
+
+    interface RGBColor {
+        r: number;
+        g: number;
+        b: number;
+    }
+
+    let progress = 0;
+    const tSpeed = (0.1 / 20) * speed;
+
+    function arraylistGradient() {
+        const arraylist = document.getElementByClassName("arraylist");
+        if (arraylist == null) return;
+        var color1 = window.getComputedStyle(arraylist).getPropertyValue("--accent-color-1").trim();
+        var color2 = window.getComputedStyle(arraylist).getPropertyValue("--accent-color-2").trim();
+        const modules = arraylist.children as HTMLCollectionOf<HTMLElement>;
+        for (let i = 0; i < modules.length; i++) {
+            const element = modules[i];
+            if (element.className != "module") continue;
+            const percentage = 1 - (i / modules.length) + (.5 * Math.sin(.5 * i + progress));;
+            const rgb = colorInterpolate(color1, color2, percentage);
+            element.style.color = `rgb( ${rgb.r}, ${rgb.g}, ${rgb.b})`;
+            element.style.borderRight = `2px solid rgb( ${rgb.r}, ${rgb.g}, ${rgb.b})`;
+        }
+    }
+
+    progress += tSpeed;
+    if (progress >= Math.PI * 2) {
+        progress = 0;
+    }
+
+    export function getRgb(color: string): RGBColor {
+        const match = color.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        if (!match) {
+          throw new Error(`Invalid color format: ${color}`);
+        }
+        return {
+          r: parseInt(match[1]),
+          g: parseInt(match[2]),
+          b: parseInt(match[3]),
+        };
+    }
+    // https://stackoverflow.com/questions/66123016/interpolate-between-two-colours-based-on-a-percentage-value
+    export function colorInterpolate(colorA: string, colorB: string, interpolation: number): RGBColor {
+        const rgbA = getRgb(colorA);
+        const rgbB = getRgb(colorB);
+
+        interpolation = Math.min(1, Math.max(0, interpolation));
+
+        const interpolateComponent = (prop: keyof RGBColor) =>
+          Math.round(rgbA[prop] * (1 - interpolation) + rgbB[prop] * interpolation);
+
+        return {
+          r: interpolateComponent('r'),
+          g: interpolateComponent('g'),
+          b: interpolateComponent('b'),
+        };
+    }
+
+    setInterval(arraylistGradient, speed)
 </script>
 
 <div class="arraylist">
