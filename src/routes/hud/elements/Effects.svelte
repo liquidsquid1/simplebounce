5ad30f9ad32b708257fdffb3ca44538c78a6d0ba
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-08-19 11:29:52 +0300

    chore: adjust effects

--- src/routes/hud/elements/Effects.svelte
+++ src/routes/hud/elements/Effects.svelte
@@ -42,8 +42,13 @@
 <style lang="scss">
   @import "../../../colors.scss";
 
+  .effects {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
   .effect {
     font-weight: 500;
+    text-shadow: $accent-color-1 0px 0px 8px;
     font-size: 14px;
     text-align: right;
 
