fbbc66ff6130286e7497bec041870594a7a09998
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-08-26 18:06:42 +0100

    feat: better looking watermark

--- src/routes/hud/elements/Watermark.svelte
+++ src/routes/hud/elements/Watermark.svelte
@@ -1,5 +1,29 @@
+<script lang="ts">
+    import {onMount} from 'svelte';
+    import {getSession} from "../../../integration/rest";
+    import {listen} from "../../../integration/ws";
+    
+    let username = "";
+
+    async function refreshSession() {
+        const session = await getSession();
+        username = session.username;
+    }
+  
+    onMount(async () => {
+        await refreshSession();
+    });
+  
+    listen("session", async () => {
+        await refreshSession();
+    });
+  </script>
+
 <span class="watermark">
-    <span class="coloured">L</span>iquidbounce
+    <span class="clientName" id="clientName">
+        liquidbounce
+    </span>
+    <span class="username">{username}</span> <span class="fakeuuid">(0000)</span>
 </span>
 
 <style lang="scss">
@@ -13,11 +37,24 @@
     }
 
     .watermark {
-        font-size: 24px;
+        font-size: 16px;
+        min-width: 350px;
+        background-color: rgba(0, 0, 0, 0.5);
+        padding: 4px;
+        border-top: 1px solid $accent-color-1;
+    }
+
+    .clientName {
+        font-weight: 500;
+        color: $accent-color-1;
+    }
+
+    .username {
         color: white;
-        text-shadow: 3px 3px #000000;
     }
-    .coloured {
-        animation: linear infinite 5s cycle;
+
+    .fakeuuid {
+        color: rgb(127, 127, 127);
     }
+
 </style>
